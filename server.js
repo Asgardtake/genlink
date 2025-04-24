87126a3df35f518ce57cc65815112e4b5aefeb22
Asgardtake <asgardtake@gmail.com>
2025-04-24 13:40:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,9 +32,10 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE, // <-- това е правилната!
   port: process.env.MYSQLPORT
 });
+
 
 
 db.connect((err) => {
