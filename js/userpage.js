1802d2afa8a925087078adae50bb860c147dce47
Asgardtake <asgardtake@gmail.com>
2025-04-28 13:24:37 +0300

    Update userpage.js

--- js/userpage.js
+++ js/userpage.js
@@ -397,5 +397,3 @@ window.addEventListener("pageshow", (event) => {
 });
 
 // Version: v1.0.2 | Last updated: 2025-04-28
-const APP_VERSION = 'v1.0.2';
-console.log(`Genlink Admin Panel - Version: ${APP_VERSION}`);
